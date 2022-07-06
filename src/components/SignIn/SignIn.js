d8af35e79c64a7f47ff6e4133f8b16fe6cfc8316
dkrol24 <krold2191@gmail.com>
2022-07-06 20:01:08 +0200

    .

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,28 +1,81 @@
-import React,{Component} from 'react'
-import Button from '../Forms/Button/Button'
-import './styles.scss'
-import {signInWithGoogle} from '../../firebase/firebase'
+import React,{Component} from 'react';
+import Button from '../Forms/Button/Button';
+import './styles.scss';
+import {signInWithGoogle,auth} from '../../firebase/firebase';
+
+import FormInput from "../Forms/FormInput/FormInput"
+
+const initialState = {
+    email: '',
+    password: '',
+}
+
+//////////////////////////////////////////////////// PAMIĘTAJ ZAPYTANIE JEŻELI EMAIL NIE ISTNIEJE ERROR
 class SignIn extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            ...initialState
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e){
+        const {name,value} = e.target;
+        this.setState({
+            [name]:value
+        });
+    }
+
     handleSubmit = async (e) =>{
         e.preventDefault();
+        const {email,password} = this.state;
+
+        try{
+
+            await auth.signInWithEmailAndPassword(email,password);
+            this.setState({
+                ...initialState
+            });
+
+        }catch(err){
+            // console.log(err);
+        }
     };
+
     render(){
+        const {email,password} = this.state;
         return(
             <div className='signin'>
                 <div className='wrap'>
                     <h2>Login</h2>
-
-<div className='formWrap'>
-    <form onSubmit={this.handleSubmit}>
-        <div className='socialSignin'>
-            <div className='row'>
-                <Button onClick={signInWithGoogle}>
-                    Sign in with Google
-                </Button>
-            </div>
-        </div>
-    </form>
-</div>
+                        <div className='formWrap'>
+                            <form onSubmit={this.handleSubmit}>
+                                <FormInput 
+                                type = "email"
+                                name = "email"
+                                value = {email}
+                                placeholder = "E-mail"
+                                handleChange={this.handleChange}
+                                />
+                                <FormInput 
+                                type = "password"
+                                name = "password"
+                                value = {password}
+                                placeholder = "Hasło"
+                                handleChange={this.handleChange}
+                                />
+                                <Button type="submit">Zaloguj</Button>
+                                <div className='socialSignin'>
+                                    <div className='row'>
+                                        <Button onClick={signInWithGoogle}>
+                                            Zaloguj przez google
+                                        </Button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
 
                 </div>
             </div>
