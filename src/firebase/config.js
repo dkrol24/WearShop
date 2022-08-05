cd81f0db0ed61d850ea06becdc70666b89e758b0
dkrol24 <krold2191@gmail.com>
2022-08-05 10:41:39 +0200

    refactored initial commit

---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+// firebase config
+export const firebaseConfig = {
+    apiKey: "AIzaSyAqpGgYhWAD2BUf4F2jMrDbFLulKLX1tGU",
+  authDomain: "wearshop2424.firebaseapp.com",
+  projectId: "wearshop2424",
+  storageBucket: "wearshop2424.appspot.com",
+  messagingSenderId: "719217982815",
+  appId: "1:719217982815:web:651938d87c1ea14cd68aea"
+};
