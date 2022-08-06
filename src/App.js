d08cb351cfadb643170d4f4d4689749c88e820f7
dkrol24 <krold2191@gmail.com>
2022-08-06 16:37:13 +0200

    admin pannel

--- src/App.js
+++ src/App.js
@@ -1,66 +1,95 @@
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { Switch, Route } from 'react-router-dom';
-import { checkUserSession } from './redux/User/user.actions';
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Switch, Route } from "react-router-dom";
+import { checkUserSession } from "./redux/User/user.actions";
+
+// components
+import AdminToolbar from "./components/AdminToolbar";
 
 // hoc
-import WithAuth from './hoc/withAuth';
-
+import WithAuth from "./hoc/WithAuth";
+import WithAdminAuth from "./hoc/withAdminAuth";
 // layouts
-import MainLayout from './layouts/MainLayout';
-import HomepageLayout from './layouts/HomepageLayout';
-
+import MainLayout from "./layouts/MainLayout";
+import HomepageLayout from "./layouts/HomepageLayout";
+import AdminLayout from "./layouts/AdminLayout";
+import DashboardLayout from "./layouts/DashboardLayout";
 // pages
-import Homepage from './pages/Homepage';
-import Registration from './pages/Registration';
-import Login from './pages/Login';
-import Recovery from './pages/Recovery';
-import Dashboard from './pages/Dashboard';
-import './default.scss';
+import Homepage from "./pages/Homepage";
+import Registration from "./pages/Registration";
+import Login from "./pages/Login";
+import Recovery from "./pages/Recovery";
+import Dashboard from "./pages/Dashboard";
+import Admin from "./pages/Admin";
+import "./default.scss";
 
-const App = props => {
+const App = (props) => {
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(checkUserSession());
-
   }, []);
 
   return (
     <div className="App">
+      <AdminToolbar />
       <Switch>
-        <Route exact path="/" render={() => (
-          <HomepageLayout>
-            <Homepage />
-          </HomepageLayout>
-        )}
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <HomepageLayout>
+              <Homepage />
+            </HomepageLayout>
+          )}
         />
-        <Route path="/registration" render={() => (
-          <MainLayout>
-            <Registration />
-          </MainLayout>
-        )} />
-        <Route path="/login"
+        <Route
+          path="/registration"
+          render={() => (
+            <MainLayout>
+              <Registration />
+            </MainLayout>
+          )}
+        />
+        <Route
+          path="/login"
           render={() => (
             <MainLayout>
               <Login />
             </MainLayout>
-          )} />
-        <Route path="/recovery" render={() => (
-          <MainLayout>
-            <Recovery />
-          </MainLayout>
-        )} />
-        <Route path="/dashboard" render={() => (
-          <WithAuth>
+          )}
+        />
+        <Route
+          path="/recovery"
+          render={() => (
             <MainLayout>
-              <Dashboard />
+              <Recovery />
             </MainLayout>
-          </WithAuth>
-        )} />
+          )}
+        />
+        <Route
+          path="/dashboard"
+          render={() => (
+            <WithAuth>
+              <DashboardLayout>
+                <Dashboard />
+              </DashboardLayout>
+            </WithAuth>
+          )}
+        />
+        <Route
+          path="/admin"
+          render={() => (
+            <WithAdminAuth>
+              <AdminLayout>
+                <Admin />
+              </AdminLayout>
+            </WithAdminAuth>
+          )}
+        />
       </Switch>
     </div>
   );
-}
+};
 
 export default App;
